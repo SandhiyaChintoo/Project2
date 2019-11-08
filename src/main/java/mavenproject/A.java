13618da1d4082eff64b36c45eb61bd3819cd5cd8
SandhiyaChintoo <sanjudhiya1994@gmail.com>
2019-11-08 22:51:53 +0530

    I done my user stories

--- src/main/java/mavenproject/A.java
+++ src/main/java/mavenproject/A.java
@@ -2,6 +2,7 @@ package mavenproject;
 
 public class A {
 public static void main(String[] args) {
+	System.out.println("Hi");
 	
 }
 }
--- src/main/java/mavenproject/B.java
+++
@@ -1,7 +0,0 @@
-package mavenproject;
-
-public class B {
-public static void main(String[] args) {
-	System.out.println("Hi");
-}
-}
